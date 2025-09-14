498e4c1d83c84034edb3be97277735eaf4727819
Damien <damien.dev2022@gmail.com>
2025-09-14 18:34:46 +0200

    feat: add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Blog-React",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
